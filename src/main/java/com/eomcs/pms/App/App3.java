1e6aa3d8830a8f6be1db754cac303afd38822020
shin-so-mi <thaleovud1@naver.com>
2020-07-17 16:33:40 +0900

    App 과제

---
+++ src/main/java/com/eomcs/pms/App/App3.java
@@ -0,0 +1,14 @@
+
+package com.eomcs.pms.App;
+
+public class App3 {
+  public static void main(String[] args) {
+
+  System.out.println("프로젝트:미니프젝트 관리 시스템 개발");
+  System.out.println("번호:1");
+  System.out.println("내용:1");
+  System.out.println("완료일:2020-01-20");
+  System.out.println("상태:진행중");
+  System.out.println("담당자:홍길동");
+  }
+}
